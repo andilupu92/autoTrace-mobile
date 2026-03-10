64375eb89b81197e634e0d6dd434fa15c2202c72
andilupu92 <andilupu92@gmail.com>
2026-03-10 17:33:04 +0200

    add edit button

--- src/page/home/cars/car.tsx
+++ src/page/home/cars/car.tsx
@@ -1,6 +1,6 @@
 import { Text } from "@/components/ui/text";
-import { View } from "react-native";
-
+import { View, TouchableOpacity } from "react-native";
+import { Ionicons } from '@expo/vector-icons'; 
 
 export default function Car({
   name,
@@ -12,8 +12,7 @@ export default function Car({
   highlight?: boolean;
 }) {
   return (
-    <View className="items-center"
-    style={!highlight ? { marginTop: -12 } : undefined}>
+    <View className="items-center" style={!highlight ? { marginTop: -12 } : undefined}>
       <View 
         className="w-20 h-20 rounded-full items-center justify-center bg-white"
         style={
@@ -26,6 +25,7 @@ export default function Car({
                 shadowOffset: { width: 0, height: 2 },
                 shadowOpacity: 0.2,
                 shadowRadius: 4,
+                position: 'relative', 
               }
             : { 
                 borderColor: '#E5E7EB',
@@ -35,10 +35,27 @@ export default function Car({
               } 
         }
       >
+        {/* Edit button */}
+        {highlight && (
+          <TouchableOpacity 
+            style={{ 
+              position: 'absolute', 
+              top: 7,
+              right: 7,
+              padding: 4,
+              opacity: 0.3,
+            }}
+            onPress={() => console.log("open sheet")}
+          >
+            <Ionicons name="pencil" size={44} color="#9CA3AF" />
+          </TouchableOpacity>
+        )}
+
         <Text className={`font-bold text-center px-1 ${highlight ? "text-orange-500 text-[13px]" : "text-gray-400 text-[10px]"}`}>
           {name}
         </Text>
       </View>
+      
       <Text className={`text-[10px] font-bold uppercase mt-3 tracking-tighter ${highlight ? "text-orange-400" : "text-gray-400"}`}>
         {km}
       </Text>
